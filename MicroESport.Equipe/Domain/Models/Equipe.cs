aa61a3f30fea92898e467ef840b8db8e42c09c98
Nicolas <v.nicolasvanhuysse@free.fr>
2023-12-12 16:39:35 +0100

    Initialisation

---
+++ MicroESport.Equipe/Domain/Models/Equipe.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MicroESport.Equipe.Domain.Models
+{
+    public class Equipe
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+    }
+}
--- MicroESport.Equipe/Program.cs
+++ MicroESport.Equipe/Program.cs
@@ -1,5 +1,15 @@
+using MongoDB.Bson;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+var mongoClient = new MongoClient(connectionString);
+var mongoDatabase = mongoClient.GetDatabase("Equipes");
+mongoDatabase.RunCommandAsync((Command<BsonDocument>)"{ping:1}").Wait(); // teste la connexion
+builder.Services.AddSingleton<IMongoDatabase>(mongoDatabase);
 
 app.MapGet("/", () => "Hello World!");
 
